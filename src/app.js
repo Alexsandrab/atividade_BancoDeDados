107e8af8280907d26423be0790cc79fc30dbf053
Alexsandrab <sandrinhaleto@hotmail.com>
2021-12-06 21:20:04 -0300

    nova api

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017", {
+  userNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Conexão feita com sucesso!");
+});
+
+const index = require("./routes/index");
+const tarefas = require("./routes/tarefasRoute");
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header(
+    "Acess-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/", index);
+app.use("/tarefas", tarefas);
+
+module.exports = app;
